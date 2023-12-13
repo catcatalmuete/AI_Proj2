59bd674057d528e38c6107b50f5b9683c11aaa18
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2023-12-12 23:52:32 -0500

    Completed code

--- CryptArith-Prob.py
+++ CryptArith-Prob.py
@@ -4,9 +4,12 @@
 # Project 2: Cryptarithmetic Problem
 # Authors: Cat Almuete, Rakeeb Hossain
 
+input_file = "Input2.txt"
+output_file = "Output2.txt"
+
 # Parse the input file and return a set of variables and a dictionary of domains
 def parse_input():
-    with open("Input1.txt", "r") as file:
+    with open(input_file, "r") as file:
         puzzle = file.read().splitlines()
 
     variables = set()
@@ -32,16 +35,18 @@ def parse_input():
     # Add carry variables and constraints to the CSP
     max_len = len(max(reversed_puzzle[0:2], key=len))
     for i in range(max_len):
-        carry_var = f'C{i+1}'
-        variables.add(carry_var)
-        domains[carry_var] = set(range(2))
+        carry1 = f'C{i}'
+        carry2 = f'C{i+1}'
+        variables.add(carry2)
+        domains[carry2] = set(range(2))
 
         # Add constraints for each column
         if i < len(reversed_puzzle[0]) and i < len(reversed_puzzle[1]) and i < len(reversed_puzzle[2]):
             constraints.append(
-                (reversed_puzzle[0][i], reversed_puzzle[1][i], reversed_puzzle[2][i], carry_var))
+                (carry1 ,reversed_puzzle[0][i], reversed_puzzle[1][i], reversed_puzzle[2][i], carry2))
 
     assignment = {letter: None for letter in variables}
+    assignment['C0'] = 0
 
     # If the length of the sum is the same as the length of the longest addend, the carry from the last column must be 0
     if len(puzzle[-1]) == max_len:
@@ -64,9 +69,6 @@ def is_complete(assignment):
 def select_unassigned_variable(variables, assignment, domains, constraints):
     unassigned_vars = [var for var in variables if assignment[var] is None]
 
-    # Minimum Remaining Values heuristic: Select variable with the fewest remaining values in its domain
-    unassigned_vars = sorted(unassigned_vars, key=lambda var: len(domains[var]))
-
     # Degree heuristic: Among variables with equal remaining values, select the one involved in the most constraints
     def degree_heuristic(var):
         count = 0
@@ -76,6 +78,12 @@ def select_unassigned_variable(variables, assignment, domains, constraints):
         return count
 
     unassigned_vars = sorted(unassigned_vars, key=degree_heuristic, reverse=True)
+
+    # Minimum Remaining Values heuristic: Select variable with the fewest remaining values in its domain
+    unassigned_vars = sorted(unassigned_vars, key=lambda var: len(domains[var]))
+
+    if (len(unassigned_vars)==len(variables)):
+        print(unassigned_vars)
 
     return unassigned_vars[0] if unassigned_vars else None
 
@@ -87,18 +95,19 @@ def order_domain_values(var, domains):
 def is_consistent(var, value, assignment, constraints):
     # Check that no other variable has been assigned this value
     for variable, assigned_value in assignment.items():
-        if variable != var and assigned_value == value:
+        if variable != var and len(variable)!=2 and len(var)!=2 and assigned_value == value:
             return False
         
     # Check that all constraints are satisfied
     for constraint in constraints:
         if var in constraint:
-            addend1 = value if var == constraint[0] else assignment.get(constraint[0], None)
-            addend2 = value if var == constraint[1] else assignment.get(constraint[1], None)
-            sumVal =  value if var == constraint[2] else assignment.get(constraint[2], None)
-            carry = value if var == constraint[3] else assignment.get(constraint[3], None)
-            if addend1 and addend2 and sumVal and carry:
-                if addend1 + addend2 != sumVal + (carry*10):
+            carry1 = value if var == constraint[0] else assignment.get(constraint[0], None)
+            addend1 = value if var == constraint[1] else assignment.get(constraint[1], None)
+            addend2 = value if var == constraint[2] else assignment.get(constraint[2], None)
+            sumVal =  value if var == constraint[3] else assignment.get(constraint[3], None)
+            carry2 = value if var == constraint[4] else assignment.get(constraint[4], None)
+            if addend1 is not None and addend2 is not None and sumVal is not None and carry1 is not None and carry2 is not None:
+                if carry1 + addend1 + addend2 != (sumVal + (carry2*10)):
                     return False
     return True
                 
@@ -135,20 +144,26 @@ def main():
     print(assignment)
     print(domains)
     print(constraints)
-    print(backtracking_search(variables, domains, assignment, constraints))
 
     # Attempt to print out to Output1.txt
     solution = backtracking_search(variables, domains, assignment, constraints)
 
     if solution is not None:
         # Create the output string in the specified format
-        output_string = ''.join(str(solution[letter]) for letter in variables if letter.isalpha())
+        output_string = ""
+        with open(input_file, "r") as file:
+            puzzle = file.read().splitlines()
+        for line in puzzle:
+            for letter in line:
+                output_string += str(assignment[letter])
+            output_string += "\n"
 
         # Write the solution to the output file
-        with open("Output1.txt", "w") as output_file:
-            output_file.write(output_string)
+        with open(output_file, "w") as file:
+            file.write(output_string)
         print("Solution written to Output1.txt")
     else:
         print("No solution found.")
+
 
 main()
